a705c0d1cf5c6623ce837cbd8c9b2b113fb86bb0
bekonyr <akmatbekuulubekmyrza@gmail.com>
2023-05-04 19:27:45 +0600

    first commit

---
+++ hover.js
@@ -0,0 +1,190 @@
+const board = document.querySelector("#board");
+const colorNames = [
+	"AliceBlue",
+	"AntiqueWhite",
+	"Aqua",
+	"Aquamarine",
+	"Azure",
+	"Beige",
+	"Bisque",
+	// "Black",
+	"BlanchedAlmond",
+	"Blue",
+	"BlueViolet",
+	"Brown",
+	"BurlyWood",
+	"CadetBlue",
+	"Chartreuse",
+	"Chocolate",
+	"Coral",
+	"CornflowerBlue",
+	"Cornsilk",
+	"Crimson",
+	"Cyan",
+	"DarkBlue",
+	"DarkCyan",
+	"DarkGoldenRod",
+	"DarkGray",
+	"DarkGrey",
+	"DarkGreen",
+	"DarkKhaki",
+	"DarkMagenta",
+	"DarkOliveGreen",
+	"DarkOrange",
+	"DarkOrchid",
+	"DarkRed",
+	"DarkSalmon",
+	"DarkSeaGreen",
+	"DarkSlateBlue",
+	"DarkSlateGray",
+	"DarkSlateGrey",
+	"DarkTurquoise",
+	"DarkViolet",
+	"DeepPink",
+	"DeepSkyBlue",
+	"DimGray",
+	"DimGrey",
+	"DodgerBlue",
+	"FireBrick",
+	"FloralWhite",
+	"ForestGreen",
+	"Fuchsia",
+	"Gainsboro",
+	"GhostWhite",
+	"Gold",
+	"GoldenRod",
+	"Gray",
+	"Grey",
+	"Green",
+	"GreenYellow",
+	"HoneyDew",
+	"HotPink",
+	"IndianRed",
+	"Indigo",
+	"Ivory",
+	"Khaki",
+	"Lavender",
+	"LavenderBlush",
+	"LawnGreen",
+	"LemonChiffon",
+	"LightBlue",
+	"LightCoral",
+	"LightCyan",
+	"LightGoldenRodYellow",
+	"LightGray",
+	"LightGrey",
+	"LightGreen",
+	"LightPink",
+	"LightSalmon",
+	"LightSeaGreen",
+	"LightSkyBlue",
+	"LightSlateGray",
+	"LightSlateGrey",
+	"LightSteelBlue",
+	"LightYellow",
+	"Lime",
+	"LimeGreen",
+	"Linen",
+	"Magenta",
+	"Maroon",
+	"MediumAquaMarine",
+	"MediumBlue",
+	"MediumOrchid",
+	"MediumPurple",
+	"MediumSeaGreen",
+	"MediumSlateBlue",
+	"MediumSpringGreen",
+	"MediumTurquoise",
+	"MediumVioletRed",
+	"MidnightBlue",
+	"MintCream",
+	"MistyRose",
+	"Moccasin",
+	"NavajoWhite",
+	"Navy",
+	"OldLace",
+	"Olive",
+	"OliveDrab",
+	"Orange",
+	"OrangeRed",
+	"Orchid",
+	"PaleGoldenRod",
+	"PaleGreen",
+	"PaleTurquoise",
+	"PaleVioletRed",
+	"PapayaWhip",
+	"PeachPuff",
+	"Peru",
+	"Pink",
+	"Plum",
+	"PowderBlue",
+	"Purple",
+	"RebeccaPurple",
+	"Red",
+	"RosyBrown",
+	"RoyalBlue",
+	"SaddleBrown",
+	"Salmon",
+	"SandyBrown",
+	"SeaGreen",
+	"SeaShell",
+	"Sienna",
+	"Silver",
+	"SkyBlue",
+	"SlateBlue",
+	"SlateGray",
+	"SlateGrey",
+	"Snow",
+	"SpringGreen",
+	"SteelBlue",
+	"Tan",
+	"Teal",
+	"Thistle",
+	"Tomato",
+	"Turquoise",
+	"Violet",
+	"Wheat",
+	"White",
+	"WhiteSmoke",
+	"Yellow",
+	"YellowGreen",
+];
+
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+
+
+for (let i = 0; i < 304; i++) {
+	const box = document.createElement("div");
+	board.append(box);
+	box.setAttribute("class", "box");
+
+	box.addEventListener("mouseover", () => setColor(box));
+	box.addEventListener("mouseout", () => removeColor(box));
+
+    btn1.addEventListener("click", () => setColor(box));
+    btn2.addEventListener("click", () => removeColor(box));
+}
+
+function setColor(el) {
+	const color = randomColor();
+	el.style.background = color;
+	el.style.transform = "scale(1.2)";
+    el.style.borderRadius = "50px";
+    el.style.transform  = "rotateY(180deg)";
+	el.style.boxShadow = `2px 2px 4px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(el) {
+	el.style.background = " #777474";
+	el.style.transform = "scale(1)";
+	el.style.boxShadow = "none";
+    el.style.borderRadius = "50px"
+    
+
+}
+
+function randomColor() {
+	const index = Math.round(Math.random() * colorNames.length);
+	return colorNames[index];
+}
